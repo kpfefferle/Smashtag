8cbf927ee9704e87ac69e42f35ce3d45a817ddb6
Kevin Pfefferle <kevin@pfefferle.co>
2016-06-11 15:07:07 -0400

    Limit RecentSearches store to 100 terms

--- Smashtag/RecentSearches.swift
+++ Smashtag/RecentSearches.swift
@@ -12,6 +12,7 @@ struct RecentSearches {
 
     private static let defaults = NSUserDefaults.standardUserDefaults()
     private static let key = "RecentSearces"
+    private static let limit = 100
 
     static var list: [String] {
         return (defaults.objectForKey(key) as? [String]) ?? []
@@ -20,6 +21,9 @@ struct RecentSearches {
     static func add(term: String) {
         var newArray = list.filter({ term.caseInsensitiveCompare($0) != .OrderedSame })
         newArray.insert(term, atIndex: 0)
+        while newArray.count > limit {
+            newArray.removeLast()
+        }
         defaults.setObject(newArray, forKey: key)
         NSLog("Updated list: \(list)")
     }
