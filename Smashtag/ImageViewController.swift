203564e5279de271fba4c8b45cb5eeaed257e7b5
Kevin Pfefferle <kevin@pfefferle.co>
2016-06-10 20:50:11 -0400

    Create custom class for new ImageVC

---
+++ Smashtag/ImageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ImageViewController.swift
+//  Smashtag
+//
+//  Created by Kevin Pfefferle on 6/10/16.
+//  Copyright © 2016 Kevin Pfefferle. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
